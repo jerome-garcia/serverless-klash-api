ad2079449cd5a7730e52bfa143f8cf29a383c9da
jerome-garcia <jpgarcia4@up.edu.ph>
2020-01-11 16:35:45 +0800

    Added CRUD APIs

---
+++ add-user.js
@@ -0,0 +1,24 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.tableName,
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      name: data.name,
+      ranking: data.ranking,
+      points: data.points,
+      createdAt: Date.now()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false });
+  }
+}
